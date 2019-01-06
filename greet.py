975e6fe0cb0ecc12e13c9232d64300bf9a5e5139
Dhruv110 <dhruv2211050@gmail.com>
2019-01-05 19:22:24 -0800

    Add files via upload
    Greet User

---
+++ greet.py
@@ -0,0 +1,10 @@
+__author__= "Dhruv Panchal"
+
+#Take input from user
+first_name = input("Enter first name : ")
+last_name = input("Enter last name : ")
+full_name = first_name +" " + last_name
+
+#Greet users
+message = ("Hello, " + full_name.title() + "!!")
+print(message)
